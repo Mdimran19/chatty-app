533379e1ee16efb572c2a26ef1187f2129bc930b
Mdimran19 <128302897+Mdimran19@users.noreply.github.com>
2025-04-18 23:42:33 +0600

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -85,10 +85,7 @@ export const useAuthStore = create((set, get) => ({
     connectSocket: () => {
         const { authUser } = get()
         if (!authUser || get().socket?.connected) return;
-        const socket = io(BASE_URL, 
-            auth: {
-              token: `Bearer ${token}`,
-                },
+        const socket = io(BASE_URL,
           {
             query: {
                 userId: authUser._id,
