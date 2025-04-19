dac05719550b1bbe4e1a3abe4b89a55e3f2fb29d
Mdimran19 <128302897+Mdimran19@users.noreply.github.com>
2025-04-19 23:59:20 +0600

    Update index.js

--- chattrix-server/index.js
+++ chattrix-server/index.js
@@ -15,7 +15,7 @@ app.use(express.json({limit: '10mb'}));
 
 app.use(express.urlencoded({limit: '10mb', extended: true })); 
 app.use(cors({
-    origin: "https://chatty-app-84qw.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
